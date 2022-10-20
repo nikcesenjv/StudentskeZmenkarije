f4421aec71f889c695de4c2e7ce60b756241be0f
Nik Česenj Vodovnik <nik.cesenj-vodovnik@hycu.com>
2022-10-20 14:58:42 +0200

    added login page

--- scripts/timer.js
+++ scripts/timer.js
@@ -4,21 +4,23 @@ function calculateTime() {
           hour = minute * 60,
           day = hour * 24;
     
-    const countDown = Date.parse("dec 12, 2022 19:00:00"),
+    const countDown = Date.parse("oct 20, 2022 13:59:00 GMT+2"),
     x = setInterval(function() {    
 
         const now = new Date().getTime(),
-            distance = countDown - now;
+              distance = countDown - now;
 
         document.getElementById("day").innerText = Math.floor(distance / (day)),
         document.getElementById("hour").innerText = Math.floor((distance % (day)) / (hour)),
         document.getElementById("minute").innerText = Math.floor((distance % (hour)) / (minute)),
         document.getElementById("second").innerText = Math.floor((distance % (minute)) / second);
 
-        if (distance < 0) {
-        document.getElementById("headline").innerText = "Dogodek se dogaja!";
-        clearInterval(x);
+        if (distance <= 750) {
+            window.location.href = "pages/loginPage.html"
+            clearInterval(x);
+            return;
         }
+
     }, 0)
 }
 
